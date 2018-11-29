7fd36f6e78dd81ba77452f3f2062f2f23564efc2
DarkNiar <38939183+DarkNiar@users.noreply.github.com>
2018-11-29 19:55:08 +0100

    Add files via upload

--- GestioneFattorini/AppDesktop3/src/Front_End.java
+++ GestioneFattorini/AppDesktop3/src/Front_End.java
@@ -24,42 +24,75 @@ public class Front_End extends JFrame {
 
     private void init() throws IOException {
 
+        //inizializzazione della tabella con il settaggio del titolo della grandezza e della chiusure del processo quando si preme la X della finestra
         this.setTitle("AppDesktop");
-        this.setSize(1000,800);
+        this.setSize(1000,510);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //creazione degli oggetti presenti nelkla parte grafica, ovvero bottoni table textfield e scrollpane
         JButton btnPacchi = new JButton("Visualizza pacchi");
         JButton btnFattorini = new JButton("Visualizza fattorini");
+
+        //creazione della scrollpane alla quale varrà passato il contenuto, in questo la caso la Table, pre creata
         JScrollPane jspFrame = new JScrollPane(myTable);
 
+        JButton btnCerca = new JButton("Cerca...");
+        JTextField txtCerca = new JTextField(20);
+
+        JButton btnRimPacco = new JButton("Rimuovi pacco");
+        JButton btnRimFatt = new JButton("Rimuovi fattorino");
+
+        //Creazione dei 3 principali panel che andrtenno a comporre la parte inferiore della finestra alle quali verranno aggiunti i vari componenti
         JPanel panel1 = new JPanel();
         panel1.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         panel1.add(btnPacchi);
         panel1.add(btnFattorini);
 
+        panel2.add(btnRimPacco);
+        panel2.add(btnRimFatt);
+        panel2.add(txtCerca);
+        panel2.add(btnCerca);
+
         this.add(jspFrame, BorderLayout.NORTH);
         this.add(panel1, BorderLayout.CENTER);
+        this.add(panel2,BorderLayout.SOUTH);
 
+        //Apertura del socket verso il server: 127.0.0.1(IP) 3103(PORT)
         Socket socket = GestioneCorriere.Util.SocketUtil.openSocket("127.0.0.1", 3103);
 
+        //aggiunto un listener che attiverà i comendi presenti al suo interno quando si premerà sul pulsante btnPacchi
         btnPacchi.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                //si svuota la Table
                 myTable.clean();
 
+                //si creano le colonne che andranno ad indicare la Tabella dei pacchi
                 String colonne[] = {"Codice del pacco","Destinatario", "Edificio", "Indirizzo", "CAP", "Paese", "Provincia", "Consegnato", "Note del fattorino", "Data di scadenza", "Codice del fattorino"};
+
+                //aggiunte alla Table le colonne che identificano i campi
                 myTable.addColumns(colonne);
 
+                //Creazione del messaggio dove viene definita l'operazione da svolgere e le condizioni di ricerca
                 Message m = new Message(DatabaseOperations.SELECT_ALL_PACKS);
                 try {
+
+                    //invio del messaggio al server
                     MessageUtil.sendMessageTo(socket, m);
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
+
+                //Creazione del contenitore del messaggio di corretta ricezione
                 Message retMessage = null;
                 try {
+
+                    //Ricezione del messaggio inviato dal server
                     retMessage = MessageUtil.recvMessageFrom(socket);
                 } catch (IOException e1) {
                     e1.printStackTrace();
@@ -67,6 +100,7 @@ public class Front_End extends JFrame {
                     e1.printStackTrace();
                 }
 
+                //Ciclo che per ogni pacco presente all'interno del db salvera in un vettore di stringe tutti i componenti del record andandoli poi ad inserire all'interno della table
                 for(Pack pack : retMessage.getResPacks()){
 
                     String[] componenti = {pack.getCodicePacco(), pack.getDestinatario(), pack.getEdificio(), pack.getIndirizzo(), pack.getCAP(), pack.getPaese(), pack.getProvincia(), String.valueOf(pack.getConsegnato()), pack.getNoteFattorino(), pack.getDataScadenza(), pack.getCodFatt()};
@@ -75,20 +109,30 @@ public class Front_End extends JFrame {
 
                 }
 
+                //chiusura del socket
                 SocketUtil.closeSocket(socket);
             }
         });
+
+        //aggiunto un listener che attiverà i comendi presenti al suo interno quando si premerà sul pulsante btnFattorini
         btnFattorini.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                //si svuota la tabella
                 myTable.clean();
 
+                //si creano le colonne che andranno ad indicare la Tabella dei Fattorini
                 String colonne[] = {"Codice del fattorino", "Cognome", "Nome", "CAP", "Paese"};
+
+                //aggiunte alla Table le colonne che identificano i campi
                 myTable.addColumns(colonne);
 
+                //Creazione del messaggio dove viene definita l'operazione da svolgere e le condizioni di ricerca
                 Message m = new Message(DatabaseOperations.SELECT_ALL_DELIVERY_MEN);
                 try {
+
+                    //invio del messaggio al server
                     MessageUtil.sendMessageTo(socket, m);
                 } catch (IOException e1) {
                     e1.printStackTrace();
@@ -102,6 +146,7 @@ public class Front_End extends JFrame {
                     e1.printStackTrace();
                 }
 
+                //Ciclo che per ogni fattorino presente all'interno del db salvera in un vettore di stringe tutti i componenti del record andandoli poi ad inserire all'interno della table
                 for(DeliveryMan deliveryMan : retMessage.getResDelMan()){
 
                     String[] componenti = {deliveryMan.getCodiceFattorino(), deliveryMan.getCognome(), deliveryMan.getNome(), deliveryMan.getCAP(), deliveryMan.getPaese()};
@@ -109,6 +154,7 @@ public class Front_End extends JFrame {
 
                 }
 
+                //chiusura del socket
                 SocketUtil.closeSocket(socket);
             }
         });
@@ -116,6 +162,7 @@ public class Front_End extends JFrame {
         this.setVisible(true);
     }
 
+    //MAIN
     public static void main(String[] args) {
 
         Front_End front_End = new Front_End();
--- GestioneFattorini/AppDesktop3/src/MyTable.java
+++ GestioneFattorini/AppDesktop3/src/MyTable.java
@@ -12,35 +12,57 @@ public class MyTable extends JTable{
 
     public void addColumns(String[] n){
 
+        //Creazione del modello della tabella
         DefaultTableModel model = (DefaultTableModel) this.getModel();
 
+        //si va a settare il nuovo modello all'interno della myTable
         this.setModel(model);
 
+        //ciclo che esegue n giri pari al numero di campi presenti nel vettore di stringhe ricevuto
         for(int i = 0; i< n.length ; i++){
 
+            //aggiunta delle colonne all'interno della Table
             model.addColumn(n[i]);
         }
 
+        //validazione della table
         this.validate();
     }
 
     public void clean() {
+
+        //Creazione del modello della tabella
         DefaultTableModel model = (DefaultTableModel) this.getModel();
 
+        //esecuzione di un ciclo che non finisce fino a quando il numero delle righe all'interno della table non + uguale a 0
         while(model.getRowCount()>0) {
+
+            //eliminazione della riga più in alto della tabella
             model.removeRow(0);
         }
+
+        //eliminazione delle colonne
         model.setColumnCount(0);
+
+        //settaggio del nuovo modello
         this.setModel(model);
+
+        //validazione del modello della tabella
         this.validate();
     }
 
     public void addRow(String[] fields) {
+
+        //Creazione del modello della tabella
         DefaultTableModel model = (DefaultTableModel) this.getModel();
 
+        //si aggiunge alla tabella la riga composta da n campi salvati nel vettore di stringhe
         model.addRow(fields);
 
+        //si va a settare il nuovo modello
         this.setModel(model);
+
+        //si valida la tabella
         this.validate();
     }
 
